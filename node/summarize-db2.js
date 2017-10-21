3cc183da4a90e2d18f90d56f49abd78ca9d8de05
Dogxx000 <scot0530@morris.umn.edu>
2017-10-20 23:01:35 -0500

    Add connection to db async

--- node/summarize-db2.js
+++ node/summarize-db2.js
@@ -11,3 +11,27 @@ var connection = mysql.createConnection(credentials);
 var dbs = new Map();
 var table = new Map();
 var records = [];
+
+async.series ([
+		function(callback) {
+			connection.connect(function (err){
+					if(err) {
+							console.log("ERROR in connecting to DB");
+							callback(); //go to callback;
+					}
+					else {
+							console.log("connected to DB");
+							callback(err); //move on to next function.
+					}
+			});
+		}
+],
+function(err) {
+		if(err) {
+				console.log("ERROR: " + err);
+		}
+		else {
+				console.log("Async Complete");
+				connection.end();
+		}
+});
